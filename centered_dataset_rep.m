a4c5b53ba983e77c2216d239b55c3e7ecd07b5e2
ivovankreveld@hotmail.com <ivovankreveld@hotmail.com>
2019-01-27 22:02:55 +0100

    more centered things

--- centered_dataset_rep.m
+++ centered_dataset_rep.m
@@ -1,9 +1,10 @@
 function ds = centered_dataset_rep(df)
     length = 24;
     margin = 4;
-    %df = df*im_bdilation();
+    %df = df*im_berosion;
+    %df = df*im_bdilation;
+    %df = df*im_rotate();
     df = df*im_box(0,1);
-    %df = im_rotate(df);
     %figure(1); show(df);
     df = df*im_resize([length-2*margin length-2*margin],'nearest');
     %figure(2); show(df);
